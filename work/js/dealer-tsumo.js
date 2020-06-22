d361efa770f550195b3ae34076fe4e5082e6660e
muramatsu <tendouzi1105@gmail.com>
2020-06-22 22:33:39 +0900

    符の計算アガリ方

--- work/js/dealer-tsumo.js
+++ work/js/dealer-tsumo.js
@@ -16,3 +16,15 @@ function getHan(){
     console.log(totalHan);
     hand.innerHTML=`${totalHan}`;
 }
+//符の計算
+function getHu(){
+    let totalHu = 20;
+    
+    const agariCheck = document.agari_form.agari;
+    for(i=0; i< agariCheck.length;i++){
+        if(agariCheck[i].checked){
+            totalHu += parseInt(agariCheck[i].value,10)
+        }
+    }
+    console.log(totalHu);
+}
